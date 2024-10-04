8d6520c1af68045ca2f35e2a48422a42f0a29b26
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2024-10-04 12:04:42 +0000

    conditionals

---
+++ 01_basics/06_conditionals.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void main(){
+    // int maths, science, history, total;
+
+    // printf("Enter the marks of science: \n");
+    // scanf("%d", &science);
+
+    // printf("Enter the marks of maths: \n");
+    // scanf("%d", &maths);
+
+    // printf("Enter the marks of history: \n");
+    // scanf("%d", &history);
+
+    // if(maths<33 || science<33 || history<33){
+    //     printf("You are failed in a individual subject \n");
+    // }
+    // else if((maths+science+history)/3 < 40){
+    //     printf("You are failed!! \n");
+    // }
+    // else{
+    //     printf("Congragulations you have passed the exam \n");
+    // }
+    // char ch;
+    // printf("Enter any character: \n");
+    // scanf("%c", &ch);
+
+    // if(ch >= 'a' && ch <= 'z'){
+    //     printf("Entered char is a lowercase \n");
+    //     printf("ASCII value of %c is %d \n", ch, ch);
+    // }
+    // else{
+    //     printf("Entered char is not a lowercase \n");
+    // }
+
+}
